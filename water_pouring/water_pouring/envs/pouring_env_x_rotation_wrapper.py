fb412864bf692ec8f5ee5cd40afa924a2440662e
Carola Stork <carola.stork@stud.tu-darmstadt.de>
2023-07-17 18:12:04 +0200

    added easier switching to prerotated env

--- water_pouring/water_pouring/envs/pouring_env_x_rotation_wrapper.py
+++ water_pouring/water_pouring/envs/pouring_env_x_rotation_wrapper.py
@@ -9,9 +9,12 @@ from scipy.spatial.transform import Rotation as R
 
 class XRotationWrapper(gym.ActionWrapper):
 
-  def __init__(self, env):
+  def __init__(self, env, prerotated=False):
     jug_start_position = [0, 0.12, -0.1] # best prerotated position: [0, 0.12, -0.1]
-    jug_start_rotation = R.from_euler('XYZ', [130, 180, 0], degrees=True) #R.from_euler('XYZ', [90, 180, 0], degrees=True) [140, 180, 0]
+    if prerotated:
+      jug_start_rotation = R.from_euler('XYZ', [130, 180, 0], degrees=True) #R.from_euler('XYZ', [90, 180, 0], degrees=True) [140, 180, 0]
+    else:
+      jug_start_rotation = R.from_euler('XYZ', [90, 180, 0], degrees=True)  
     jug_start_position.extend(jug_start_rotation.as_quat())
 
     #env.jug_start_position = jug_start_position
@@ -27,4 +30,4 @@ class XRotationWrapper(gym.ActionWrapper):
     self.action_space = spaces.Box(low=-1, high=1)
 
   def action(self, act):
-          return [np.zeros(3), [act, 0, 0]]
+          return [np.zeros(3), [act[0], 0, 0]]
